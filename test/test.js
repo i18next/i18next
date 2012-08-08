15af3f79d0030097108e716a7f81ae53d4a048c3
Jan Muehlemann <jan.muehlemann@gmail.com>
2012-08-08 11:39:27 +0200

    support arrays as translation string - merge with new line

--- test/test.js
+++ test/test.js
@@ -473,6 +473,23 @@ describe('i18next', function() {
 
   describe('translation functionality', function() {
 
+    describe('resource string as array', function() {
+      var resStore = {
+        dev: { translation: { testarray: ["title", "text"] } },
+        en: { translation: { } },            
+        'en-US': { translation: { } }
+      };
+      
+      beforeEach(function(done) {
+        i18n.init( $.extend(opts, { resStore: resStore }),
+          function(t) { done(); });
+      });
+
+      it('it should translate nested value', function() {
+        expect(i18n.t('testarray')).to.be('title\ntext');
+      });
+    });
+
     describe('accessing nested values', function() {
 
       beforeEach(function(done) {
