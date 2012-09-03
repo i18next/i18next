beb0552771980e36fcdddbdbcbe87976ab9b133d
Jan Muehlemann <jan.muehlemann@gmail.com>
2012-09-03 14:44:31 +0200

    make detect language safer - test for window, document

--- src/i18next.detectLanguage.js
+++ src/i18next.detectLanguage.js
@@ -3,24 +3,26 @@ function detectLanguage() {
 
     // get from qs
     var qsParm = [];
-    (function() {
-        var query = window.location.search.substring(1);
-        var parms = query.split('&');
-        for (var i=0; i<parms.length; i++) {
-            var pos = parms[i].indexOf('=');
-            if (pos > 0) {
-                var key = parms[i].substring(0,pos);
-                var val = parms[i].substring(pos+1);
-                qsParm[key] = val;
+    if (window) {
+        (function() {
+            var query = window.location.search.substring(1);
+            var parms = query.split('&');
+            for (var i=0; i<parms.length; i++) {
+                var pos = parms[i].indexOf('=');
+                if (pos > 0) {
+                    var key = parms[i].substring(0,pos);
+                    var val = parms[i].substring(pos+1);
+                    qsParm[key] = val;
+                }
             }
+        })();
+        if (qsParm.setLng) {
+            detectedLng = qsParm.setLng;
         }
-    })();
-    if (qsParm.setLng) {
-        detectedLng = qsParm.setLng;
     }
 
     // get from cookie
-    if (!detectedLng) {
+    if (!detectedLng && document) {
         var c = f.cookie.read('i18next');
         if (c) detectedLng = c;
     }
