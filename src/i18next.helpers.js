7924df4bfec2c987768aac8355a6efcb0ece0534
Andrey Larionov <anlarionov@gmail.com>
2012-09-06 18:53:09 +0400

    Safe browser checks. Noop object for cookies

--- src/i18next.helpers.js
+++ src/i18next.helpers.js
@@ -329,6 +329,40 @@ function _ajax(options) {
         }
     });
 }
+var _cookie = {
+    create: function(name,value,minutes) {
+        var expires;
+        if (minutes) {
+            var date = new Date();
+            date.setTime(date.getTime()+(minutes*60*1000));
+            expires = "; expires="+date.toGMTString();
+        }
+        else expires = "";
+        document.cookie = name+"="+value+expires+"; path=/";
+    },
+
+    read: function(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    },
+
+    remove: function(name) {
+        this.create(name,"",-1);
+    }
+};
+var cookie_noop = {
+    create: function(name,value,minutes) {},
+    read: function(name) { return null; },
+    remove: function(name) {}
+};
+
+
 
 // move dependent functions to a container so that
 // they can be overriden easier in no jquery environment (node.js)
@@ -336,36 +370,10 @@ var f = {
     extend: $ ? $.extend : _extend,
     each: $ ? $.each : _each,
     ajax: $ ? $.ajax : _ajax,
+    cookie: typeof document !== 'undefined' ? _cookie : cookie_noop,
     detectLanguage: detectLanguage,
     log: function(str) {
         if (o.debug) console.log(str);
-    },
-    cookie: {
-        create: function(name,value,minutes) {
-            var expires;
-            if (minutes) {
-                var date = new Date();
-                date.setTime(date.getTime()+(minutes*60*1000));
-                expires = "; expires="+date.toGMTString();
-            }
-            else expires = "";
-            document.cookie = name+"="+value+expires+"; path=/";
-        },
-        
-        read: function(name) {
-            var nameEQ = name + "=";
-            var ca = document.cookie.split(';');
-            for(var i=0;i < ca.length;i++) {
-                var c = ca[i];
-                while (c.charAt(0)==' ') c = c.substring(1,c.length);
-                if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
-            }
-            return null;
-        },
-        
-        remove: function(name) {
-                this.create(name,"",-1);
-        }
     },
     toLanguages: function(lng) {
         var languages = [];
