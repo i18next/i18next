6d28203a83d66f797c5d5ce86f4c9997aaa215bd
Daniel Pehrson <dpehrson@qurve.com>
2013-09-20 17:46:32 -0400

    Add options to array-key tests just to make sure the expectation is clear that all potential keys must use the same options.

--- test/test.js
+++ test/test.js
@@ -1165,7 +1165,7 @@ describe('i18next', function() {
   
     describe('resource key as array', function() {
       var resStore = {
-        dev: { translation: { existing1: 'hello', existing2: 'world' } },
+        dev: { translation: { existing1: 'hello _name_', existing2: 'howdy __name__' } },
         en: { translation: { } },
         'en-US': { translation: { } }
       };
@@ -1177,19 +1177,19 @@ describe('i18next', function() {
     
       describe('when none of the keys exist', function() {
         it('return the same value as translating the last non-existent key', function() {
-          expect(i18n.t(['nonexistent1', 'nonexistent2'])).to.equal(i18n.t('nonexistent2'));
+          expect(i18n.t(['nonexistent1', 'nonexistent2'], {name: "Joe"})).to.equal(i18n.t('nonexistent2', {name: "Joe"}));
         });
       });
     
       describe('when one of the keys exist', function() {
         it('return the same value as translating the one existing key', function() {
-          expect(i18n.t(['nonexistent1', 'existing2'])).to.equal(i18n.t('existing2'));
+          expect(i18n.t(['nonexistent1', 'existing2'], {name: "Joe"})).to.equal(i18n.t('existing2', {name: "Joe"}));
         });
       });
     
       describe('when two or more of the keys exist', function() {
         it('return the same value as translating the first existing key', function() {
-          expect(i18n.t(['nonexistent1', 'existing2', 'existing1'])).to.equal(i18n.t('existing2'));
+          expect(i18n.t(['nonexistent1', 'existing2', 'existing1'], {name: "Joe"})).to.equal(i18n.t('existing2', {name: "Joe"}));
         });
       });
     });
