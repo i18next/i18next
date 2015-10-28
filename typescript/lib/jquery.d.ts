f9d479b78e9a8c5894c4b72debfd5cb519a15811
Ats Uiboupin <ats.uiboupin@iglu.ee>
2015-10-28 13:58:05 +0200

    #421, fixes #448 - replaced `bool` type with `boolean` to remove errors related to using i18next in TypeScript projects (fixes 87 errors produced by TypeScript compiler by running `tsc typescript\i18next.d.ts`)

--- typescript/lib/jquery.d.ts
+++ typescript/lib/jquery.d.ts
@@ -20,33 +20,33 @@ and limitations under the License.
 */
 interface JQueryAjaxSettings {
     accepts?: any;
-    async?: bool;
+    async?: boolean;
     beforeSend?(jqXHR: JQueryXHR, settings: JQueryAjaxSettings);
-    cache?: bool;
+    cache?: boolean;
     complete?(jqXHR: JQueryXHR, textStatus: string);
     contents?: { [key: string]: any; };
     contentType?: string;
     context?: any;
     converters?: { [key: string]: any; };
-    crossDomain?: bool;
+    crossDomain?: boolean;
     data?: any;
     dataFilter?(data: any, ty: any): any;
     dataType?: string;
     error?(jqXHR: JQueryXHR, textStatus: string, errorThrow: string): any;
-    global?: bool;
+    global?: boolean;
     headers?: { [key: string]: any; };
-    ifModified?: bool;
-    isLocal?: bool;
+    ifModified?: boolean;
+    isLocal?: boolean;
     jsonp?: string;
     jsonpCallback?: any;
     mimeType?: string;
     password?: string;
-    processData?: bool;
+    processData?: boolean;
     scriptCharset?: string;
     statusCode?: { [key: string]: any; };
     success?(data: any, textStatus: string, jqXHR: JQueryXHR);
     timeout?: number;
-    traditional?: bool;
+    traditional?: boolean;
     type?: string;
     url?: string;
     username?: string;
@@ -69,11 +69,11 @@ interface JQueryCallback {
     disable(): any;
     empty(): any;
     fire(...arguments: any[]): any;
-    fired(): bool;
+    fired(): boolean;
     fireWith(context: any, ...args: any[]): any;
-    has(callback: any): bool;
+    has(callback: any): boolean;
     lock(): any;
-    locked(): bool;
+    locked(): boolean;
     remove(...callbacks: any[]): any;
 }
 
@@ -114,9 +114,9 @@ interface JQueryDeferred extends JQueryPromise {
 interface JQueryEventObject extends Event {
     data: any;
     delegateTarget: Element;
-    isDefaultPrevented(): bool;
-    isImmediatePropogationStopped(): bool;
-    isPropogationStopped(): bool;
+    isDefaultPrevented(): boolean;
+    isImmediatePropogationStopped(): boolean;
+    isPropogationStopped(): boolean;
     namespace: string;
     preventDefault(): any;
     relatedTarget: Element;
@@ -133,34 +133,34 @@ interface JQueryEventObject extends Event {
     Collection of properties of the current browser
 */
 interface JQueryBrowserInfo {
-    safari:bool;
-    opera:bool;
-    msie:bool;
-    mozilla:bool;
-    webkit:bool;
+    safari: boolean;
+    opera: boolean;
+    msie: boolean;
+    mozilla: boolean;
+    webkit: boolean;
     version:string;
 }
 
 interface JQuerySupport {
-    ajax?: bool;
-    boxModel?: bool;
-    changeBubbles?: bool;
-    checkClone?: bool;
-    checkOn?: bool;
-    cors?: bool;
-    cssFloat?: bool;
-    hrefNormalized?: bool;
-    htmlSerialize?: bool;
-    leadingWhitespace?: bool;
-    noCloneChecked?: bool;
-    noCloneEvent?: bool;
-    opacity?: bool;
-    optDisabled?: bool;
-    optSelected?: bool;
-    scriptEval?(): bool;
-    style?: bool;
-    submitBubbles?: bool;
-    tbody?: bool;
+    ajax?: boolean;
+    boxModel?: boolean;
+    changeBubbles?: boolean;
+    checkClone?: boolean;
+    checkOn?: boolean;
+    cors?: boolean;
+    cssFloat?: boolean;
+    hrefNormalized?: boolean;
+    htmlSerialize?: boolean;
+    leadingWhitespace?: boolean;
+    noCloneChecked?: boolean;
+    noCloneEvent?: boolean;
+    opacity?: boolean;
+    optDisabled?: boolean;
+    optSelected?: boolean;
+    scriptEval?(): boolean;
+    style?: boolean;
+    submitBubbles?: boolean;
+    tbody?: boolean;
 }
 
 /*
@@ -186,7 +186,7 @@ interface JQueryStatic {
     getScript(url: string, success?: any): JQueryXHR;
 
     param(obj: any): string;
-    param(obj: any, traditional: bool): string;
+    param(obj: any, traditional: boolean): string;
 
     post(url: string, data?: any, success?: any, dataType?: any): JQueryXHR;
 
@@ -198,7 +198,7 @@ interface JQueryStatic {
     /****
      CORE
     *****/
-    holdReady(hold: bool): any;
+    holdReady(hold: boolean): any;
 
     (selector: string, context?: any): JQuery;
     (element: Element): JQuery;
@@ -209,7 +209,7 @@ interface JQueryStatic {
     (array: any[]): JQuery;
     (): JQuery;
 
-    noConflict(removeAll?: bool): Object;
+    noConflict(removeAll?: boolean): Object;
 
     when(...deferreds: any[]): JQueryPromise;
 
@@ -230,7 +230,7 @@ interface JQueryStatic {
 
     dequeue(element: Element, queueName?: string): any;
 
-    hasData(element: Element): bool;
+    hasData(element: Element): boolean;
 
     queue(element: Element, queueName?: string): any[];
     queue(element: Element, queueName: string, newQueueOrCallback: any): JQuery;
@@ -240,7 +240,7 @@ interface JQueryStatic {
     /*******
      EFFECTS
     ********/
-    fx: { tick: () => void; interval: number; stop: () => void; speeds: { slow: number; fast: number; }; off: bool; step: any; };
+    fx: { tick: () => void; interval: number; stop: () => void; speeds: { slow: number; fast: number; }; off: boolean; step: any; };
 
     /******
      EVENTS
@@ -259,7 +259,7 @@ interface JQueryStatic {
     **************/
     expr: any;
     fn: any;  //TODO: Decide how we want to type this
-    isReady: bool;
+    isReady: boolean;
 
     /**********
      PROPERTIES
@@ -270,26 +270,26 @@ interface JQueryStatic {
     /*********
      UTILITIES
     **********/
-    contains(container: Element, contained: Element): bool;
+    contains(container: Element, contained: Element): boolean;
 
     each(collection: any, callback: (indexInArray: any, valueOfElement: any) => any): any;
 
     extend(target: any, ...objs: any[]): Object;
-    extend(deep: bool, target: any, ...objs: any[]): Object;
+    extend(deep: boolean, target: any, ...objs: any[]): Object;
 
     globalEval(code: string): any;
 
-    grep(array: any[], func: any, invert?: bool): any[];
+    grep(array: any[], func: any, invert?: boolean): any[];
 
     inArray(value: any, array: any[], fromIndex?: number): number;
 
-    isArray(obj: any): bool;
-    isEmptyObject(obj: any): bool;
-    isFunction(obj: any): bool;
-    isNumeric(value: any): bool;
-    isPlainObject(obj: any): bool;
-    isWindow(obj: any): bool;
-    isXMLDoc(node: Node): bool;
+    isArray(obj: any): boolean;
+    isEmptyObject(obj: any): boolean;
+    isFunction(obj: any): boolean;
+    isNumeric(value: any): boolean;
+    isPlainObject(obj: any): boolean;
+    isWindow(obj: any): boolean;
+    isXMLDoc(node: Node): boolean;
 
     makeArray(obj: any): any[];
 
@@ -345,7 +345,7 @@ interface JQuery {
     attr(map: { [key: string]: any; }): JQuery;
     attr(attributeName: string, func: (index: any, attr: any) => any): JQuery;
 
-    hasClass(className: string): bool;
+    hasClass(className: string): boolean;
 
     html(): string;
     html(htmlString: string): JQuery;
@@ -363,8 +363,8 @@ interface JQuery {
 
     removeProp(propertyName: any): JQuery;
 
-    toggleClass(className: any, swtch?: bool): JQuery;
-    toggleClass(swtch?: bool): JQuery;
+    toggleClass(className: any, swtch?: boolean): JQuery;
+    toggleClass(swtch?: boolean): JQuery;
     toggleClass(func: (index: any, cls: any, swtch: any) => any): JQuery;
 
     val(): any;
@@ -391,8 +391,8 @@ interface JQuery {
     offset(coordinates: any): JQuery;
     offset(func: (index: any, coords: any) => any): JQuery;
 
-    outerHeight(includeMargin?: bool): number;
-    outerWidth(includeMargin?: bool): number;
+    outerHeight(includeMargin?: boolean): number;
+    outerWidth(includeMargin?: boolean): number;
 
     position(): { top: number; left: number; };
 
@@ -430,7 +430,7 @@ interface JQuery {
     ********/
     animate(properties: any, duration?: any, complete?: Function): JQuery;
     animate(properties: any, duration?: any, easing?: string, complete?: Function): JQuery;
-    animate(properties: any, options: { duration?: any; easing?: string; complete?: Function; step?: Function; queue?: bool; specialEasing?: any; });
+    animate(properties: any, options: { duration?: any; easing?: string; complete?: Function; step?: Function; queue?: boolean; specialEasing?: any; });
 
     delay(duration: number, queueName?: string): JQuery;
 
@@ -461,19 +461,19 @@ interface JQuery {
     slideUp(duration?: any, callback?: any): JQuery;
     slideUp(duration?: any, easing?: string, callback?: any): JQuery;
 
-    stop(clearQueue?: bool, jumpToEnd?: bool): JQuery;
-    stop(queue?:any, clearQueue?: bool, jumpToEnd?: bool): JQuery;
+    stop(clearQueue?: boolean, jumpToEnd?: boolean): JQuery;
+    stop(queue?:any, clearQueue?: boolean, jumpToEnd?: boolean): JQuery;
 
     toggle(duration?: any, callback?: any): JQuery;
     toggle(duration?: any, easing?: string, callback?: any): JQuery;
-    toggle(showOrHide: bool): JQuery;
+    toggle(showOrHide: boolean): JQuery;
 
     /******
      EVENTS
     *******/
     bind(eventType: string, eventData?: any, handler?: (eventObject: JQueryEventObject) => any): JQuery;
-    bind(eventType: string, eventData: any, preventBubble:bool): JQuery;
-    bind(eventType: string, preventBubble:bool): JQuery;
+    bind(eventType: string, eventData: any, preventBubble: boolean): JQuery;
+    bind(eventType: string, preventBubble: boolean): JQuery;
     bind(...events: any[]);
 
     blur(eventData?: any, handler?: (eventObject: JQueryEventObject) => any): JQuery;
@@ -574,7 +574,7 @@ interface JQuery {
     triggerHandler(eventType: string, ...extraParameters: any[]): Object;
 
     unbind(eventType?: string, handler?: (eventObject: JQueryEventObject) => any): JQuery;
-    unbind(eventType: string, fls: bool): JQuery;
+    unbind(eventType: string, fls: boolean): JQuery;
     unbind(evt: any): JQuery;
 
     undelegate(): JQuery;
@@ -612,7 +612,7 @@ interface JQuery {
     before(...content: any[]): JQuery;
     before(func: (index: any) => any);
 
-    clone(withDataAndEvents?: bool, deepWithDataAndEvents?: bool): JQuery;
+    clone(withDataAndEvents?: boolean, deepWithDataAndEvents?: boolean): JQuery;
 
     detach(selector?: any): JQuery;
 
@@ -704,10 +704,10 @@ interface JQuery {
     has(selector: string): JQuery;
     has(contained: Element): JQuery;
 
-    is(selector: string): bool;
-    is(func: (index: any) =>any): bool;
-    is(element: any): bool;
-    is(obj: JQuery): bool;
+    is(selector: string): boolean;
+    is(func: (index: any) =>any): boolean;
+    is(element: any): boolean;
+    is(obj: JQuery): boolean;
 
     last(): JQuery;
 
