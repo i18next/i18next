9d6ae4647854662ec5683c99c601920d09d4c7ff
Ray Bateman <rayb@gradecam.com>
2016-02-24 11:19:36 -0700

    Fixes a problem when using both context and pluralization. It now will fallback to default plural if no context is found. Possible key list order: key_context_plural key_context key_plural key

--- test/translator/translator.translate.combination.spec.js
+++ test/translator/translator.translate.combination.spec.js
@@ -15,7 +15,13 @@ describe('Translator', () => {
           translation: {
             'key1': 'hello world',
             'key2': 'It is: $t(key1)',
-            'key3': 'It is: {{val}}'
+            'key3': 'It is: {{val}}',
+
+            // context with pluralization
+            test: 'test_en',
+            test_plural: 'tests_en',
+            test_male: 'test_male_en',
+            test_male_plural: 'tests_male_en',
           }
         }
       });
@@ -26,6 +32,7 @@ describe('Translator', () => {
         pluralResolver: new PluralResolver(lu, {prepend: '_'}),
         interpolator: new Interpolator()
       }, {
+        contextSeparator: '_',
         ns: 'translation',
         defaultNS: 'translation',
         interpolation: {}
@@ -36,7 +43,13 @@ describe('Translator', () => {
     var tests = [
       // interpolation and nesting in var
       { args: ['key2'], expected: 'It is: hello world' },
-      { args: ['key3', { val: '$t(key1)' }], expected: 'It is: hello world' }
+      { args: ['key3', { val: '$t(key1)' }], expected: 'It is: hello world' },
+
+      // context with pluralization
+      {args: ['test', { context: 'unknown', count: 1 }], expected: 'test_en'},
+      {args: ['test', { context: 'unknown', count: 2 }], expected: 'tests_en'},
+      {args: ['test', { context: 'male', count: 1 }], expected: 'test_male_en'},
+      {args: ['test', { context: 'male', count: 2 }], expected: 'tests_male_en'},
     ];
 
     tests.forEach((test) => {
