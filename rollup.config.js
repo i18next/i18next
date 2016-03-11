60955fa4719c315783811eaea8b493d98255bda6
Jan Muehlemann <jan.muehlemann@gmail.com>
2016-03-11 09:54:30 +0100

    rebuild

--- rollup.config.js
+++ rollup.config.js
@@ -9,6 +9,7 @@ const compress = argv.uglify;
 const babelOptions = {
   exclude: 'node_modules/**',
   presets: ['es2015-rollup', 'stage-0'],
+  plugins: ['transform-proto-to-assign'],
   babelrc: false
 };
 
