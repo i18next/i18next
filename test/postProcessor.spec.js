d521f2f5caecd471dfd0b30633e8632d9de5a912
Jan Muehlemann <jan.muehlemann@gmail.com>
2017-05-12 21:57:04 +0200

    adds postProcessor test

---
+++ test/postProcessor.spec.js
@@ -0,0 +1,27 @@
+import postProcessor from '../src/postProcessor';
+
+describe('postProcessor', () => {
+
+  describe('add and handle()', () => {
+
+    before(() => {
+      postProcessor.addPostProcessor({
+        name: 'dummy',
+        process: (value, key, options, translator) => {
+          return value.toUpperCase();
+        }
+      });
+    });
+
+    var tests = [
+      {args: [['dummy'], 'test', 'key', {}, () => {}], expected: 'TEST'}
+    ];
+
+    tests.forEach((test) => {
+      it('correctly process for ' + JSON.stringify(test.args) + ' args', () => {
+        expect(postProcessor.handle.apply(postProcessor, test.args)).to.eql(test.expected);
+      });
+    });
+  });
+
+});
