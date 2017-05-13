2656fa49b7a6367a94c97a2985a3432492a9e330
Jan Muehlemann <jan.muehlemann@gmail.com>
2017-05-13 22:04:27 +0200

    extend backend tests

--- test/backend/backendConnector.load.retry.spec.js
+++ test/backend/backendConnector.load.retry.spec.js
@@ -24,3 +24,69 @@ describe('BackendConnector load retry', () => {
     });
   });
 });
+
+describe('BackendConnector load retry on multi', () => {
+  let connector;
+
+  before(() => {
+    connector = new BackendConnector(new BackendMock(), new ResourceStore(), {
+      interpolator: new Interpolator()
+    }, {
+      backend: {
+        allowMultiLoading: true,
+        loadPath: 'http://localhost:9876/locales/{{lng}}/{{ns}}.json'
+      }
+    });
+  });
+
+  describe('#load', () => {
+    it('should load multi data', (done) => {
+      connector.load(['de', 'fr'], ['retry', 'other'], function(err) {
+        expect(err.length).to.equal(3);
+        expect(connector.store.getResourceBundle('de', 'retry')).to.eql({status: 'nok', retries: 2});
+        done();
+      });
+    });
+  });
+});
+
+describe('BackendConnector reload retry', () => {
+  let connector;
+
+  before(() => {
+    connector = new BackendConnector(new BackendMock(), new ResourceStore(), {
+      interpolator: new Interpolator()
+    }, {
+      backend: { loadPath: 'http://localhost:9876/locales/{{lng}}/{{ns}}.json' }
+    });
+  });
+
+  describe('#reload', () => {
+    it('should reload data', () => {
+      connector.reload(['es'], ['noretry']);
+      expect(connector.store.getResourceBundle('es', 'noretry')).to.eql({status: 'nok', retries: 0});
+    });
+  });
+});
+
+describe('BackendConnector reload retry on multi', () => {
+  let connector;
+
+  before(() => {
+    connector = new BackendConnector(new BackendMock(), new ResourceStore(), {
+      interpolator: new Interpolator()
+    }, {
+      backend: {
+        allowMultiLoading: true,
+        loadPath: 'http://localhost:9876/locales/{{lng}}/{{ns}}.json'
+      }
+    });
+  });
+
+  describe('#reload', () => {
+    it('should reload multi data', () => {
+      connector.reload(['it', 'fr'], ['noretry', 'other']);
+      expect(connector.store.getResourceBundle('it', 'noretry')).to.eql({status: 'nok', retries: 0});
+    });
+  });
+});
--- test/backend/backendMock.js
+++ test/backend/backendMock.js
@@ -21,6 +21,22 @@ class Backend {
       return;
     }
   }
+
+  readMulti(languages, namespaces, callback) {
+    const language = languages[0];
+    const namespace = namespaces[0];
+
+    if (!this.retries[language]) this.retries[language] = 0;
+
+    if (namespace === 'retry' && this.retries[language] < 2) {
+      this.retries[language]++;
+      return callback('failed loading', true);
+    } else {
+      callback(null, {[language]: { [namespace]: {status: 'nok', retries: this.retries[language]}}});
+      delete this.retries[language];
+      return;
+    }
+  }
 }
 
 
