f3a2497e9f88a754f6a9b1bf385cf7e3288b790a
Jan Muehlemann <jan.muehlemann@gmail.com>
2017-05-13 11:55:49 +0200

    add emitter test

---
+++ test/eventEmitter.spec.js
@@ -0,0 +1,31 @@
+import EventEmitter from '../src/EventEmitter';
+
+describe('i18next', () => {
+  let emitter;
+
+  before(() => {
+    emitter = new EventEmitter();
+  });
+
+  describe('published', () => {
+    it('it should convert options', (done) => {
+      // test on
+      emitter.on('ok', (payload) => {
+        expect(payload).to.equal('data ok');
+        done();
+      });
+
+      // test off
+      const nok = (payload) => {
+        expect(payload).to.equal('not called as off');
+        done();
+      }
+      emitter.on('nok', nok);
+      emitter.off('nok', nok);
+
+      emitter.emit('nok', 'there should be no listener');
+      emitter.emit('ok', 'data ok');
+    });
+  });
+
+});
