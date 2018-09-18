635e3b6103365a76478990081bfcecff8bc48ad2
ossdev07 <39188636+ossdev07@users.noreply.github.com>
2018-09-18 16:42:03 +0530

    Update karma.backward.conf.js

--- karma.backward.conf.js
+++ karma.backward.conf.js
@@ -24,7 +24,7 @@ module.exports = function(karma) {
       customLaunchers: {
         HeadlessChrome: {
           base: 'ChromeHeadless',
-          flags: [ '—no-sandbox’, ],
+          flags: [ '—no-sandbox', ],
          },
       },
 
