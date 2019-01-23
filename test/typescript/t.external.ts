922e5897dab0ac2e27813771500d1e9827a3aba6
Kevin Ross <kevin.ross@alienfast.com>
2019-01-23 16:18:45 -0600

    Comment this pattern applies to NamespacesConsumer
    https://github.com/i18next/react-i18next/issues/693

--- test/typescript/t.external.ts
+++ test/typescript/t.external.ts
@@ -1,7 +1,8 @@
 import i18next from 'i18next';
 
 /**
- * Use of the exported TFunction in external utility methods
+ * Use of the exported TFunction in external utility methods such as
+ *  NamespaceConsumer children t
  */
 function resolveText(t: i18next.TFunction, keyOrText?: string): undefined | string {
   if (keyOrText && keyOrText.startsWith(':')) {
