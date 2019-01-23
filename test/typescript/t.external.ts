63a59c790a9d737ba0954f9d60648421b6718edd
Kevin Ross <kevin.ross@alienfast.com>
2019-01-23 16:52:36 -0600

    Parameterized use of TFunction fails while WithT use works

--- test/typescript/t.external.ts
+++ test/typescript/t.external.ts
@@ -11,3 +11,17 @@ function resolveText(t: i18next.TFunction, keyOrText?: string): undefined | stri
     return keyOrText;
   }
 }
+
+function childrenNamespacesConsumer(t: i18next.TFunction, i18n: i18next.i18n) {
+  // sanity first - tests from i18next t.test
+  const is: string = i18n.t('friend'); // same as <string>
+  const io: object = i18n.t<object>('friend');
+  const isa: string[] = i18n.t<string[]>('friend');
+  const ioa: object[] = i18n.t<object[]>('friend');
+
+  // now try t provided by NamespacesConsumer
+  const s: string = t('friend'); // same as <string>
+  const o: object = t<object>('friend');
+  const sa: string[] = t<string[]>('friend');
+  const oa: object[] = t<object[]>('friend');
+}
