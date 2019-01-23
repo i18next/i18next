32b4af1acea71a2b3ce44d18005a9ea8ed68ff4b
Kevin Ross <kevin.ross@alienfast.com>
2019-01-23 09:20:03 -0600

    ignore and revert prettier changes to PluralResolver

--- src/PluralResolver.js
+++ src/PluralResolver.js
@@ -3,260 +3,74 @@ import baseLogger from './logger.js';
 // definition http://translate.sourceforge.net/wiki/l10n/pluralforms
 /* eslint-disable */
 let sets = [
-  {
-    lngs: [
-      'ach',
-      'ak',
-      'am',
-      'arn',
-      'br',
-      'fil',
-      'gun',
-      'ln',
-      'mfe',
-      'mg',
-      'mi',
-      'oc',
-      'pt',
-      'pt-BR',
-      'tg',
-      'ti',
-      'tr',
-      'uz',
-      'wa',
-    ],
-    nr: [1, 2],
-    fc: 1,
-  },
+  { lngs: ['ach','ak','am','arn','br','fil','gun','ln','mfe','mg','mi','oc', 'pt', 'pt-BR',
+    'tg','ti','tr','uz','wa'], nr: [1,2], fc: 1 },
 
-  {
-    lngs: [
-      'af',
-      'an',
-      'ast',
-      'az',
-      'bg',
-      'bn',
-      'ca',
-      'da',
-      'de',
-      'dev',
-      'el',
-      'en',
-      'eo',
-      'es',
-      'et',
-      'eu',
-      'fi',
-      'fo',
-      'fur',
-      'fy',
-      'gl',
-      'gu',
-      'ha',
-      'hi',
-      'hu',
-      'hy',
-      'ia',
-      'it',
-      'kn',
-      'ku',
-      'lb',
-      'mai',
-      'ml',
-      'mn',
-      'mr',
-      'nah',
-      'nap',
-      'nb',
-      'ne',
-      'nl',
-      'nn',
-      'no',
-      'nso',
-      'pa',
-      'pap',
-      'pms',
-      'ps',
-      'pt-PT',
-      'rm',
-      'sco',
-      'se',
-      'si',
-      'so',
-      'son',
-      'sq',
-      'sv',
-      'sw',
-      'ta',
-      'te',
-      'tk',
-      'ur',
-      'yo',
-    ],
-    nr: [1, 2],
-    fc: 2,
-  },
+  { lngs: ['af','an','ast','az','bg','bn','ca','da','de','dev','el','en',
+    'eo','es','et','eu','fi','fo','fur','fy','gl','gu','ha','hi',
+    'hu','hy','ia','it','kn','ku','lb','mai','ml','mn','mr','nah','nap','nb',
+    'ne','nl','nn','no','nso','pa','pap','pms','ps','pt-PT','rm','sco',
+    'se','si','so','son','sq','sv','sw','ta','te','tk','ur','yo'], nr: [1,2], fc: 2 },
 
-  {
-    lngs: [
-      'ay',
-      'bo',
-      'cgg',
-      'fa',
-      'id',
-      'ja',
-      'jbo',
-      'ka',
-      'kk',
-      'km',
-      'ko',
-      'ky',
-      'lo',
-      'ms',
-      'sah',
-      'su',
-      'th',
-      'tt',
-      'ug',
-      'vi',
-      'wo',
-      'zh',
-    ],
-    nr: [1],
-    fc: 3,
-  },
+  { lngs: ['ay','bo','cgg','fa','id','ja','jbo','ka','kk','km','ko','ky','lo',
+    'ms','sah','su','th','tt','ug','vi','wo','zh'], nr: [1], fc: 3 },
 
-  { lngs: ['be', 'bs', 'dz', 'hr', 'ru', 'sr', 'uk'], nr: [1, 2, 5], fc: 4 },
+  { lngs: ['be','bs','dz','hr','ru','sr','uk'], nr: [1,2,5], fc: 4 },
 
-  { lngs: ['ar'], nr: [0, 1, 2, 3, 11, 100], fc: 5 },
-  { lngs: ['cs', 'sk'], nr: [1, 2, 5], fc: 6 },
-  { lngs: ['csb', 'pl'], nr: [1, 2, 5], fc: 7 },
-  { lngs: ['cy'], nr: [1, 2, 3, 8], fc: 8 },
-  { lngs: ['fr'], nr: [1, 2], fc: 9 },
-  { lngs: ['ga'], nr: [1, 2, 3, 7, 11], fc: 10 },
-  { lngs: ['gd'], nr: [1, 2, 3, 20], fc: 11 },
-  { lngs: ['is'], nr: [1, 2], fc: 12 },
-  { lngs: ['jv'], nr: [0, 1], fc: 13 },
-  { lngs: ['kw'], nr: [1, 2, 3, 4], fc: 14 },
-  { lngs: ['lt'], nr: [1, 2, 10], fc: 15 },
-  { lngs: ['lv'], nr: [1, 2, 0], fc: 16 },
-  { lngs: ['mk'], nr: [1, 2], fc: 17 },
-  { lngs: ['mnk'], nr: [0, 1, 2], fc: 18 },
-  { lngs: ['mt'], nr: [1, 2, 11, 20], fc: 19 },
-  { lngs: ['or'], nr: [2, 1], fc: 2 },
-  { lngs: ['ro'], nr: [1, 2, 20], fc: 20 },
-  { lngs: ['sl'], nr: [5, 1, 2, 3], fc: 21 },
-  { lngs: ['he'], nr: [1, 2, 20, 21], fc: 22 },
-];
+  { lngs: ['ar'], nr: [0,1,2,3,11,100], fc: 5 },
+  { lngs: ['cs','sk'], nr: [1,2,5], fc: 6 },
+  { lngs: ['csb','pl'], nr: [1,2,5], fc: 7 },
+  { lngs: ['cy'], nr: [1,2,3,8], fc: 8 },
+  { lngs: ['fr'], nr: [1,2], fc: 9 },
+  { lngs: ['ga'], nr: [1,2,3,7,11], fc: 10 },
+  { lngs: ['gd'], nr: [1,2,3,20], fc: 11 },
+  { lngs: ['is'], nr: [1,2], fc: 12 },
+  { lngs: ['jv'], nr: [0,1], fc: 13 },
+  { lngs: ['kw'], nr: [1,2,3,4], fc: 14 },
+  { lngs: ['lt'], nr: [1,2,10], fc: 15 },
+  { lngs: ['lv'], nr: [1,2,0], fc: 16 },
+  { lngs: ['mk'], nr: [1,2], fc: 17 },
+  { lngs: ['mnk'], nr: [0,1,2], fc: 18 },
+  { lngs: ['mt'], nr: [1,2,11,20], fc: 19 },
+  { lngs: ['or'], nr: [2,1], fc: 2 },
+  { lngs: ['ro'], nr: [1,2,20], fc: 20 },
+  { lngs: ['sl'], nr: [5,1,2,3], fc: 21 },
+  { lngs: ['he'], nr: [1,2,20,21], fc: 22 }
+]
 
 let _rulesPluralsTypes = {
-  1: function(n) {
-    return Number(n > 1);
-  },
-  2: function(n) {
-    return Number(n != 1);
-  },
-  3: function(n) {
-    return 0;
-  },
-  4: function(n) {
-    return Number(
-      n % 10 == 1 && n % 100 != 11
-        ? 0
-        : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20)
-        ? 1
-        : 2,
-    );
-  },
-  5: function(n) {
-    return Number(
-      n === 0
-        ? 0
-        : n == 1
-        ? 1
-        : n == 2
-        ? 2
-        : n % 100 >= 3 && n % 100 <= 10
-        ? 3
-        : n % 100 >= 11
-        ? 4
-        : 5,
-    );
-  },
-  6: function(n) {
-    return Number(n == 1 ? 0 : n >= 2 && n <= 4 ? 1 : 2);
-  },
-  7: function(n) {
-    return Number(
-      n == 1 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2,
-    );
-  },
-  8: function(n) {
-    return Number(n == 1 ? 0 : n == 2 ? 1 : n != 8 && n != 11 ? 2 : 3);
-  },
-  9: function(n) {
-    return Number(n >= 2);
-  },
-  10: function(n) {
-    return Number(n == 1 ? 0 : n == 2 ? 1 : n < 7 ? 2 : n < 11 ? 3 : 4);
-  },
-  11: function(n) {
-    return Number(n == 1 || n == 11 ? 0 : n == 2 || n == 12 ? 1 : n > 2 && n < 20 ? 2 : 3);
-  },
-  12: function(n) {
-    return Number(n % 10 != 1 || n % 100 == 11);
-  },
-  13: function(n) {
-    return Number(n !== 0);
-  },
-  14: function(n) {
-    return Number(n == 1 ? 0 : n == 2 ? 1 : n == 3 ? 2 : 3);
-  },
-  15: function(n) {
-    return Number(
-      n % 10 == 1 && n % 100 != 11 ? 0 : n % 10 >= 2 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2,
-    );
-  },
-  16: function(n) {
-    return Number(n % 10 == 1 && n % 100 != 11 ? 0 : n !== 0 ? 1 : 2);
-  },
-  17: function(n) {
-    return Number(n == 1 || n % 10 == 1 ? 0 : 1);
-  },
-  18: function(n) {
-    return Number(n == 0 ? 0 : n == 1 ? 1 : 2);
-  },
-  19: function(n) {
-    return Number(
-      n == 1
-        ? 0
-        : n === 0 || (n % 100 > 1 && n % 100 < 11)
-        ? 1
-        : n % 100 > 10 && n % 100 < 20
-        ? 2
-        : 3,
-    );
-  },
-  20: function(n) {
-    return Number(n == 1 ? 0 : n === 0 || (n % 100 > 0 && n % 100 < 20) ? 1 : 2);
-  },
-  21: function(n) {
-    return Number(n % 100 == 1 ? 1 : n % 100 == 2 ? 2 : n % 100 == 3 || n % 100 == 4 ? 3 : 0);
-  },
-  22: function(n) {
-    return Number(n === 1 ? 0 : n === 2 ? 1 : (n < 0 || n > 10) && n % 10 == 0 ? 2 : 3);
-  },
+  1: function(n) {return Number(n > 1);},
+  2: function(n) {return Number(n != 1);},
+  3: function(n) {return 0;},
+  4: function(n) {return Number(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);},
+  5: function(n) {return Number(n===0 ? 0 : n==1 ? 1 : n==2 ? 2 : n%100>=3 && n%100<=10 ? 3 : n%100>=11 ? 4 : 5);},
+  6: function(n) {return Number((n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2);},
+  7: function(n) {return Number(n==1 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);},
+  8: function(n) {return Number((n==1) ? 0 : (n==2) ? 1 : (n != 8 && n != 11) ? 2 : 3);},
+  9: function(n) {return Number(n >= 2);},
+  10: function(n) {return Number(n==1 ? 0 : n==2 ? 1 : n<7 ? 2 : n<11 ? 3 : 4) ;},
+  11: function(n) {return Number((n==1 || n==11) ? 0 : (n==2 || n==12) ? 1 : (n > 2 && n < 20) ? 2 : 3);},
+  12: function(n) {return Number(n%10!=1 || n%100==11);},
+  13: function(n) {return Number(n !== 0);},
+  14: function(n) {return Number((n==1) ? 0 : (n==2) ? 1 : (n == 3) ? 2 : 3);},
+  15: function(n) {return Number(n%10==1 && n%100!=11 ? 0 : n%10>=2 && (n%100<10 || n%100>=20) ? 1 : 2);},
+  16: function(n) {return Number(n%10==1 && n%100!=11 ? 0 : n !== 0 ? 1 : 2);},
+  17: function(n) {return Number(n==1 || n%10==1 ? 0 : 1);},
+  18: function(n) {return Number(n==0 ? 0 : n==1 ? 1 : 2);},
+  19: function(n) {return Number(n==1 ? 0 : n===0 || ( n%100>1 && n%100<11) ? 1 : (n%100>10 && n%100<20 ) ? 2 : 3);},
+  20: function(n) {return Number(n==1 ? 0 : (n===0 || (n%100 > 0 && n%100 < 20)) ? 1 : 2);},
+  21: function(n) {return Number(n%100==1 ? 1 : n%100==2 ? 2 : n%100==3 || n%100==4 ? 3 : 0); },
+  22: function(n) {return Number(n===1 ? 0 : n===2 ? 1 : (n<0 || n>10) && n%10==0 ? 2 : 3); }
 };
 /* eslint-enable */
 
 function createRules() {
   const rules = {};
-  sets.forEach(set => {
-    set.lngs.forEach(l => {
+  sets.forEach((set) => {
+    set.lngs.forEach((l) => {
       rules[l] = {
         numbers: set.nr,
-        plurals: _rulesPluralsTypes[set.fc],
+        plurals: _rulesPluralsTypes[set.fc]
       };
     });
   });
@@ -294,7 +108,7 @@ class PluralResolver {
 
     if (!rule) return ret;
 
-    rule.numbers.forEach(n => {
+    rule.numbers.forEach((n) => {
       const suffix = this.getSuffix(code, n);
       ret.push(`${key}${suffix}`);
     });
@@ -320,10 +134,9 @@ class PluralResolver {
         }
       }
 
-      const returnSuffix = () =>
-        this.options.prepend && suffix.toString()
-          ? this.options.prepend + suffix.toString()
-          : suffix.toString();
+      const returnSuffix = () => (
+        this.options.prepend && suffix.toString() ? this.options.prepend + suffix.toString() : suffix.toString()
+      );
 
       // COMPATIBILITY JSON
       // v1
@@ -333,16 +146,10 @@ class PluralResolver {
         return returnSuffix();
       } else if (/* v2 */ this.options.compatibilityJSON === 'v2') {
         return returnSuffix();
-      } else if (
-        /* v3 - gettext index */ this.options.simplifyPluralSuffix &&
-        rule.numbers.length === 2 &&
-        rule.numbers[0] === 1
-      ) {
+      } else if (/* v3 - gettext index */ this.options.simplifyPluralSuffix && rule.numbers.length === 2 && rule.numbers[0] === 1) {
         return returnSuffix();
       }
-      return this.options.prepend && idx.toString()
-        ? this.options.prepend + idx.toString()
-        : idx.toString();
+      return this.options.prepend && idx.toString() ? this.options.prepend + idx.toString() : idx.toString();
     }
 
     this.logger.warn(`no plural rule found for: ${code}`);
