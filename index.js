69feb81c25cfa0de78a1cc8ec841911e0c5859e2
Bogdan Chadkin <trysound@yandex.ru>
2019-06-03 12:47:01 +0300

    Bundle all entry points with rollup
    Similar to https://github.com/i18next/i18next-chained-backend/pull/8

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* eslint no-var: 0 */
-var main = require('./dist/commonjs/index.js').default;
+var main = require('./dist/cjs/i18next.js').default;
 
 module.exports = main;
 module.exports.default = main;
