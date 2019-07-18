0b37ce3b2564a102ebbedb1aeb9b3b95568c13b9
PierreAndreis <pierreandreisortega@gmail.com>
2019-07-18 10:33:08 -0700

    interop module + tests

---
+++ test/typescript/interopModule.test.ts
@@ -0,0 +1,14 @@
+// This file uses <root>/tsconfig.interopModule.json typescript configuration
+// VSCode doesn't support a specific tsconfig for a specific file,
+// so this code will fail on vscode lint. It works on tests
+import i18next, { Interpolator, WithT, Modules } from 'i18next';
+
+i18next.init();
+
+const interpolator: Interpolator = i18next.services.interpolator;
+
+const mockWithT: WithT = {
+  t: ((key: string) => key) as any,
+};
+
+const modules: Modules = { external: [] };
