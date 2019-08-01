56f5ea9d5f58763a50f2eaf8965789c3d248a724
Kevin Ross <kevin.ross@alienfast.com>
2019-08-01 14:30:49 -0500

    Expose more classes in rollup bundle for downstream testing
    Needed for https://github.com/i18next/i18next-xhr-backend/pull/319

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import i18next from './i18next.js';
 
 export default i18next;
+
+// expose the following in the rollup bundle for downstream testing e.g. i18next-xhr-backend
+export { default as BackendConnector } from './BackendConnector';
+export { default as Interpolator } from './Interpolator';
+export { default as ResourceStore } from './ResourceStore';
