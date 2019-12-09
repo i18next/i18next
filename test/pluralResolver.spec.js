23014f725c1c0c5b41c50f3587538c6a4eaf603c
Eldad Bercovici <dutzi.b@gmail.com>
2019-12-09 11:54:40 +0200

    fix tests

--- test/pluralResolver.spec.js
+++ test/pluralResolver.spec.js
@@ -194,16 +194,10 @@ describe('PluralResolver', () => {
       { args: ['sl', 3], expected: '3' },
 
       // he
-      { args: ['he', 0], expected: '3' },
-      { args: ['he', 0.5], expected: '3' },
-      { args: ['he', 1], expected: '0' },
-      { args: ['he', 2], expected: '1' },
-      { args: ['he', 3], expected: '3' },
-      { args: ['he', 20], expected: '2' },
-      { args: ['he', 21], expected: '3' },
-      { args: ['he', 30], expected: '2' },
-      { args: ['he', 100], expected: '2' },
-      { args: ['he', 101], expected: '3' },
+      { args: ['he', 0], expected: 'plural' },
+      { args: ['he', 1], expected: '' },
+      { args: ['he', 10], expected: 'plural' },
+      { args: ['he', 10.5], expected: 'plural' },
     ];
 
     tests.forEach(test => {
