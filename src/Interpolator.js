0fbf74a2c5c1e4a594f3b2ed62bb8da254958d0d
Brett Moran <brett.moran@msn.com>
2020-03-10 18:49:56 +1100

    Allow for formatting of translations.

--- src/Interpolator.js
+++ src/Interpolator.js
@@ -185,6 +185,21 @@ class Interpolator {
 
     // regular escape on demand
     while ((match = this.nestingRegexp.exec(str))) {
+      let formatters = [];
+
+      /**
+       * If there is more than one parameter (contains the format separator). E.g.:
+       *   - t(a, b)
+       *   - t(a, b, c)
+       *
+       * And those parameters are not dynamic values (parameters do not include curly braces). E.g.:
+       *   - Not t(a, { "key": "{{variable}}" })
+       *   - Not t(a, b, {"keyA": "valueA", "keyB": "valueB"})
+       */
+      if (match[0].includes(this.formatSeparator) && !/{.*}/.test(match[1])) {
+        [match[1], ...formatters] = match[1].split(this.formatSeparator).map(elem => elem.trim());
+      }
+
       value = fc(handleHasOptions.call(this, match[1].trim(), clonedOptions), clonedOptions);
 
       // is only the nesting key (key1 = '$(key2)') return the value without stringify
@@ -196,6 +211,9 @@ class Interpolator {
         this.logger.warn(`missed to resolve ${match[1]} for nesting ${str}`);
         value = '';
       }
+
+      value = formatters.reduce((v, f) => this.format(v, f, options.lng, options), value.trim());
+
       // Nested keys should not be escaped by default #854
       // value = this.escapeValue ? regexSafe(utils.escape(value)) : regexSafe(value);
       str = str.replace(match[0], value);
