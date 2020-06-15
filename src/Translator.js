0131c82710ea19173a7e8944dd5fd33161bd573a
Adriano Raiano <adrai@users.noreply.github.com>
2020-06-15 16:41:57 +0200

    make log warning more clear

--- src/Translator.js
+++ src/Translator.js
@@ -190,7 +190,7 @@ class Translator extends EventEmitter {
         );
         if (keySeparator) {
           const fk = this.resolve(key, { ...options, keySeparator: false });
-          if (fk && fk.res) this.logger.warn('Are translations flat instead of nested?');
+          if (fk && fk.res) this.logger.warn('Seems the loaded translations were in flat JSON format instead of nested. Either set keySeparator: false on init or make sure your translations are published in nested format.');
         }
 
         let lngs = [];
