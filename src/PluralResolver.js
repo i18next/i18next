8d17fb17ed8551956c502edc7473e15649ce13e9
Jan Mühlemann <jan.muehlemann@gmail.com>
2021-02-19 12:40:18 +0100

    add tl -> Tagalog to plural rules

--- src/PluralResolver.js
+++ src/PluralResolver.js
@@ -4,7 +4,7 @@ import baseLogger from './logger.js';
 /* eslint-disable */
 let sets = [
   { lngs: ['ach','ak','am','arn','br','fil','gun','ln','mfe','mg','mi','oc', 'pt', 'pt-BR',
-    'tg','ti','tr','uz','wa'], nr: [1,2], fc: 1 },
+    'tg', 'tl', 'ti','tr','uz','wa'], nr: [1,2], fc: 1 },
 
   { lngs: ['af','an','ast','az','bg','bn','ca','da','de','dev','el','en',
     'eo','es','et','eu','fi','fo','fur','fy','gl','gu','ha','hi',
