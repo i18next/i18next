i18next/i18next

commit 448e2be33042ed959d762eaff270fa804e2baaf4

Reduce CPU usage loading large numbers of items (#1768)

src/BackendConnector.js
@@ -2,13 +2,9 @@ import * as utils from './utils.js';
 import baseLogger from './logger.js';
 import EventEmitter from './EventEmitter.js';
 
-function remove(arr, what) {
-  let found = arr.indexOf(what);
-
-  while (found !== -1) {
-    arr.splice(found, 1);
-    found = arr.indexOf(what);
-  }
+function removePending(q, name) {
+  delete q.pending[name];
+  q.pendingCount--;
 }
 
 class Connector extends EventEmitter {
@@ -35,10 +31,10 @@ class Connector extends EventEmitter {
 
   queueLoad(languages, namespaces, options, callback) {
     // find what needs to be loaded
-    const toLoad = [];
-    const pending = [];
-    const toLoadLanguages = [];
-    const toLoadNamespaces = [];
+    const toLoad = {};
+    const pending = {};
+    const toLoadLanguages = {};
+    const toLoadNamespaces = {};
 
     languages.forEach((lng) => {
       let hasAllNamespaces = true;
@@ -51,24 +47,25 @@ class Connector extends EventEmitter {
         } else if (this.state[name] < 0) {
           // nothing to do for err
         } else if (this.state[name] === 1) {
-          if (pending.indexOf(name) < 0) pending.push(name);
+          if (pending[name] !== undefined) pending[name] = true;
         } else {
           this.state[name] = 1; // pending
 
           hasAllNamespaces = false;
 
-          if (pending.indexOf(name) < 0) pending.push(name);
-          if (toLoad.indexOf(name) < 0) toLoad.push(name);
-          if (toLoadNamespaces.indexOf(ns) < 0) toLoadNamespaces.push(ns);
+          pending[name] = true;
+          toLoad[name] = true;
+          toLoadNamespaces[ns] = true;
         }
       });
 
-      if (!hasAllNamespaces) toLoadLanguages.push(lng);
+      if (!hasAllNamespaces) toLoadLanguages[lng] = true;
     });
 
-    if (toLoad.length || pending.length) {
+    if (Object.keys(toLoad).length || Object.keys(pending).length) {
       this.queue.push({
         pending,
+        pendingCount: Object.keys(pending).length,
         loaded: {},
         errors: [],
         callback,
@@ -76,10 +73,10 @@ class Connector extends EventEmitter {
     }
 
     return {
-      toLoad,
-      pending,
-      toLoadLanguages,
-      toLoadNamespaces,
+      toLoad: Object.keys(toLoad),
+      pending: Object.keys(pending),
+      toLoadLanguages: Object.keys(toLoadLanguages),
+      toLoadNamespaces: Object.keys(toLoadNamespaces),
     };
   }
 
@@ -103,17 +100,18 @@ class Connector extends EventEmitter {
     // callback if ready
     this.queue.forEach((q) => {
       utils.pushPath(q.loaded, [lng], ns);
-      remove(q.pending, name);
+      removePending(q, name);
 
       if (err) q.errors.push(err);
 
-      if (q.pending.length === 0 && !q.done) {
+      if (q.pendingCount === 0 && !q.done) {
         // only do once per loaded -> this.emit('loaded', q.loaded);
         Object.keys(q.loaded).forEach((l) => {
-          if (!loaded[l]) loaded[l] = [];
-          if (q.loaded[l].length) {
-            q.loaded[l].forEach((ns) => {
-              if (loaded[l].indexOf(ns) < 0) loaded[l].push(ns);
+          if (!loaded[l]) loaded[l] = {};
+          const loadedKeys = Object.keys(loaded[l]);
+          if (loadedKeys.length) {
+            loadedKeys.forEach((ns) => {
+              if (loadedKeys[ns] !== undefined) loaded[l][ns] = true;
             });
           }
         });

test/backend/backendConnector.perf.spec.js
@@ -1,0 +1,39 @@
+import BackendConnector from '../../src/BackendConnector.js';
+import BackendMock from './backendMock.js';
+import Interpolator from '../../src/Interpolator.js';
+import ResourceStore from '../../src/ResourceStore.js';
+import { expect } from 'chai';
+
+describe('BackendConnector performance test', () => {
+  let connector;
+
+  before(() => {
+    connector = new BackendConnector(
+      new BackendMock(),
+      new ResourceStore(),
+      {
+        interpolator: new Interpolator(),
+      },
+      {
+        backend: { loadPath: 'http://localhost:9876/locales/{{lng}}/{{ns}}.json' },
+      },
+    );
+  });
+
+  describe('#load', () => {
+    it('should load 10,000 items in under the 2 second timeout', (done) => {
+      const namespaces = [];
+      for (let i = 0; i < 10000; i++) {
+        namespaces.push(`namespace${i}`);
+      }
+      connector.load(['en'], namespaces, function (err) {
+        expect(err).to.be.not.ok;
+        expect(connector.store.getResourceBundle('en', 'namespace1')).to.eql({
+          status: 'nok',
+          retries: 0,
+        });
+        done();
+      });
+    });
+  });
+});

commit bd2fa9aa0e525ad439e9944e40868451695a0406

fix regression introduced with #1768, fixes #1772 and #1773

i18next.js
@@ -2026,13 +2026,13 @@
             if (!options.reload && _this2.store.hasResourceBundle(lng, ns)) {
               _this2.state[name] = 2;
             } else if (_this2.state[name] < 0) ; else if (_this2.state[name] === 1) {
-              if (pending[name] !== undefined) pending[name] = true;
+              if (pending[name] === undefined) pending[name] = true;
             } else {
               _this2.state[name] = 1;
               hasAllNamespaces = false;
-              pending[name] = true;
-              toLoad[name] = true;
-              toLoadNamespaces[ns] = true;
+              if (pending[name] === undefined) pending[name] = true;
+              if (toLoad[name] === undefined) toLoad[name] = true;
+              if (toLoadNamespaces[ns] === undefined) toLoadNamespaces[ns] = true;
             }
           });
           if (!hasAllNamespaces) toLoadLanguages[lng] = true;

src/BackendConnector.js
@@ -51,15 +51,15 @@ class Connector extends EventEmitter {
         } else if (this.state[name] < 0) {
           // nothing to do for err
         } else if (this.state[name] === 1) {
-          if (pending[name] !== undefined) pending[name] = true;
+          if (pending[name] === undefined) pending[name] = true;
         } else {
           this.state[name] = 1; // pending
 
           hasAllNamespaces = false;
 
-          pending[name] = true;
-          toLoad[name] = true;
-          toLoadNamespaces[ns] = true;
+          if (pending[name] === undefined) pending[name] = true;
+          if (toLoad[name] === undefined) toLoad[name] = true;
+          if (toLoadNamespaces[ns] === undefined) toLoadNamespaces[ns] = true;
         }
       });
 

test/backend/backendMock.js
@@ -15,6 +15,11 @@ class Backend {
     if (namespace === 'retry' && this.retries[language] < 2) {
       this.retries[language]++;
       return callback('failed loading', true);
+    } else if (namespace.indexOf('concurrently') === 0) {
+      setTimeout(() => {
+        callback(null, { status: 'ok', namespace });
+      }, 200);
+      return;
     } else {
       callback(null, { status: 'nok', retries: this.retries[language] });
       delete this.retries[language];
@@ -40,5 +45,7 @@ class Backend {
     }
   }
 }
+
+Backend.type = 'backend';
 
 export default Backend;

test/i18next.backend.concurrentLoad.spec.js
@@ -1,0 +1,32 @@
+import i18next from '../src/i18next.js';
+import BackendMock from './backend/backendMock.js';
+
+describe('i18next backend', () => {
+  const i18n = i18next.createInstance();
+
+  before(() => {
+    i18n.use(BackendMock).init({
+      fallbackLng: 'en',
+      fallbackNS: 'concurrently',
+      ns: [],
+      defaultNS: '',
+    });
+  });
+
+  describe('loadNamespaces concurrently', () => {
+    it('should load data correctly', (done) => {
+      let finished = 0;
+      const loadedClb = (index) =>
+        function () {
+          finished += index;
+          if (i18n.t('status') !== 'ok')
+            return done(`Expected "${i18n.t('status')}" to equal "ok"`);
+          if (finished === 3) done();
+        };
+      setTimeout(() => {
+        i18n.loadNamespaces(['concurrently']).then(loadedClb(1));
+        i18n.loadNamespaces(['concurrently']).then(loadedClb(2));
+      }, 0);
+    });
+  });
+});