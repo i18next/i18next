bb7a9a83dd3948a7fbee08bf6715718290d32cc6
Adriano Raiano <adriano@raiano.ch>
2022-12-09 11:02:03 +0100

    extend example

--- examples/typescript/index.ts
+++ examples/typescript/index.ts
@@ -6,4 +6,4 @@ console.log(i18next.t('ns1:title'));
 console.log(i18next.t('ns1:description.part1'));
 console.log(i18next.t('ns1:description.part2'));
 console.log(i18next.t('ns2:description.part2'));
-// console.log(i18next.t('description.part2', { ns: 'ns2' }));
+console.log(i18next.t('description.part2', { ns: 'ns2' }));
