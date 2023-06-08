ada2013c426dcb5290911b03aca182b888c4d019
Adriano Raiano <adriano@raiano.ch>
2023-06-08 11:11:22 +0200

    cleanup test

--- test/pluralResolver.spec.js
+++ test/pluralResolver.spec.js
@@ -174,7 +174,6 @@ describe('PluralResolver', () => {
       { compatibilityJSON: 'v2', expected: false },
       { compatibilityJSON: 'v3', expected: false },
       { compatibilityJSON: 'v4', expected: true },
-      { compatibilityJSON: 'v5', expected: true },
       { expected: true },
     ];
 
