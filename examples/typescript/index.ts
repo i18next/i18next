b7075f2ede932ac9f08dee4b5d99f6a22d848735
Adriano Raiano <adriano@raiano.ch>
2023-06-10 07:44:07 +0200

    ts example: seems compilerOptions needs to be strict

--- examples/typescript/index.ts
+++ examples/typescript/index.ts
@@ -2,7 +2,7 @@ import i18next from 'i18next';
 import './i18n/config';
 
 console.log(i18next.t('ns1:title'));
-// console.log(i18next.t('title'));
+console.log(i18next.t('title'));
 console.log(i18next.t('ns1:description.part1'));
 console.log(i18next.t('ns1:description.part2'));
 console.log(i18next.t('ns2:description.part2'));
