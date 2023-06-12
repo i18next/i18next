b5cba98e9494fd2f4dde8f120e91e927d2c4d83a
Adriano Raiano <adriano@raiano.ch>
2023-06-12 22:10:07 +0200

    example: extend with i18next-resources-for-ts

--- examples/typescript/@types/i18next.d.ts
+++ examples/typescript/@types/i18next.d.ts
@@ -1,18 +1,20 @@
 import { resources, defaultNS } from '../i18n/config';
+import Resources from './resources';
 
 declare module 'i18next' {
   interface CustomTypeOptions {
     defaultNS: typeof defaultNS;
-    resources: (typeof resources)['en'];
+    resources: Resources;
+    // resources: (typeof resources)['en'];
     // resources: {
-    //   ns1: {
-    //     "title": "Welcome!",
-    //     "description": {
-    //       "part1": "This is just a basic example of how to use i18next with typescript",
-    //       "part2": "😉"
-    //     },
-    //     "inter": "interpolated {{val}}"
-    //   };
+    //   // ns1: {
+    //   //   "title": "Welcome!",
+    //   //   "description": {
+    //   //     "part1": "This is just a basic example of how to use i18next with typescript",
+    //   //     "part2": "😉"
+    //   //   },
+    //   //   "inter": "interpolated {{val}}"
+    //   // };
     //   // ns1: typeof resources['en']['ns1'];
     //   ns2: typeof resources['en']['ns2'];
     // };
