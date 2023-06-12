52e5712f1cddd6da08338fa2a29bca5c169b2725
Adriano Raiano <adriano@raiano.ch>
2023-06-12 20:40:16 +0200

    example: ts

--- examples/typescript/index.ts
+++ examples/typescript/index.ts
@@ -8,6 +8,7 @@ console.log(i18next.t('ns1:description.part2'));
 console.log(i18next.t('ns2:description.part2'));
 console.log(i18next.t('description.part2', { ns: 'ns2' }));
 
+// interpolation
 i18next.t('inter', { val: 'xx' });
 // @ts-expect-error
 i18next.t('inter', { valWrong: 'xx' });
