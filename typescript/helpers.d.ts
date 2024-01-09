821d2648b019a7932d2d7959a307e406fcb9c6a7
Sylvain Reucherand <sylvain.reucherand@gmail.com>
2024-01-09 16:24:05 +0100

    feat(i18next): typescript joinArrays support

--- typescript/helpers.d.ts
+++ typescript/helpers.d.ts
@@ -3,3 +3,10 @@ export type $Dictionary<T = unknown> = { [key: string]: T };
 export type $OmitArrayKeys<Arr> = Arr extends readonly any[] ? Omit<Arr, keyof any[]> : Arr;
 export type $PreservedValue<Value, Fallback> = [Value] extends [never] ? Fallback : Value;
 export type $SpecialObject = object | Array<string | object>;
+export type $PrependIfDefined<T extends string, S extends string> = T extends '' ? T : `${S}${T}`;
+export type $ConcatArray<T, S extends string, U = never> = T extends readonly [
+  infer F extends string,
+  ...infer R extends string[],
+]
+  ? `${F}${$PrependIfDefined<$ConcatArray<R, S, ''>, S>}`
+  : U;
