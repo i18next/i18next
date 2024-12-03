6070d1365b4430f65fca7cac20b61e2ce182ed09
Julian Grinblat <julian@dotcore.co.il>
2024-12-03 13:05:17 +0900

    fix: incorrect locale detected

--- src/LanguageUtils.js
+++ src/LanguageUtils.js
@@ -31,7 +31,16 @@ class LanguageUtil {
   formatLanguageCode(code) {
     // http://www.iana.org/assignments/language-tags/language-tags.xhtml
     if (isString(code) && code.indexOf('-') > -1) {
-      let formattedCode = Intl.getCanonicalLocales(code)[0];
+      let formattedCode;
+      try {
+        formattedCode = Intl.getCanonicalLocales(code)[0];
+      } catch (e) {
+        // this can happen if the detected language is not valid
+        // for example: if the path detector is used and the requested path is `/providers`
+        if (!(e instanceof RangeError) || e.message !== 'Incorrect locale information provided') {
+          throw e;
+        }
+      }
       if (formattedCode && this.options.lowerCaseLng) {
         formattedCode = formattedCode.toLowerCase();
       }
