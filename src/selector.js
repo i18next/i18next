2bf99f3686adaea62ffdf58a4eeba6f018aae466
Andrew Jarrett <ahrjarrett@gmail.com>
2025-06-18 16:49:50 -0500

    refactor: removes ns logic from `keyFromSelector`

--- src/selector.js
+++ src/selector.js
@@ -1,17 +1,5 @@
 export const PATH_KEY = Symbol.for('@i18next/PATH_KEY');
 
-function join(path, opts) {
-  return path.reduce((acc, cur) => {
-    if (acc.endsWith(opts.nsSeparator)) {
-      return `${acc.slice(0, -opts.nsSeparator.length)}${opts.nsSeparator}${cur}`;
-    }
-    if (acc.length === 0) {
-      return `${cur}`;
-    }
-    return `${acc}.${cur}`;
-  }, '');
-}
-
 export function createProxy() {
   const state = Array.of();
   const handler = Object.create(null);
@@ -33,7 +21,7 @@ export function createProxy() {
   return Proxy.revocable(Object.create(null), handler).proxy;
 }
 
-export function keysFromSelector(selector, opts) {
+export function keysFromSelector(selector) {
   const { [PATH_KEY]: PATH } = selector(createProxy());
-  return join(PATH, opts);
+  return PATH.join('.');
 }
--- test/runtime/selector.test.js
+++ test/runtime/selector.test.js
@@ -1,35 +1,21 @@
 import { describe, it, expect } from 'vitest';
 import { keysFromSelector } from '../../src/selector.js';
 
-const defaultOptions = {
-  nsSeparator: ':',
-};
-
 describe('keysFromSelector', () => {
   describe('example-based test suite', () => {
     it('handles identity function', () => {
-      const keys = keysFromSelector(($) => $, defaultOptions);
+      const keys = keysFromSelector(($) => $);
       expect(keys).toEqual('');
     });
 
     it('tracks property access with default key separator', () => {
-      const keys = keysFromSelector(($) => $.a.b.c, defaultOptions);
+      const keys = keysFromSelector(($) => $.a.b.c);
       expect(keys).toEqual('a.b.c');
     });
 
     it('works with array indices', () => {
-      const keys = keysFromSelector(($) => $[0][1][2], defaultOptions);
+      const keys = keysFromSelector(($) => $[0][1][2]);
       expect(keys).toEqual('0.1.2');
-    });
-
-    it('tracks property access with default namespace separator', () => {
-      const keys = keysFromSelector(($) => $['ns:'].a.b.c, defaultOptions);
-      expect(keys).toEqual('ns:a.b.c');
-    });
-
-    it('works with custom namespace separator', () => {
-      const keys = keysFromSelector(($) => $['ns::'].a.b.c, { nsSeparator: '::' });
-      expect(keys).toEqual('ns::a.b.c');
     });
   });
 });
