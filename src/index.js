ab084ce186e891364e923253c78c55f836bb3ee2
andrew jarrett <ahrjarrett@gmail.com>
2025-09-04 00:41:57 -0500

    exports `keyFromSelector` function for testing purposes (#2346)
    * chore: exports `keysFromSelector` function for testing purposes
    
    * chore: renames `keysFromSelector` to `keyFromSelector` so the name is less confusing
    
    * export type declaration for `keyFromSelector`
    
    * allow user to specify the type of translations if they want

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import i18next from './i18next.js';
 
+export { default as keyFromSelector } from './selector.js';
+
 export default i18next;
 
 export const createInstance = i18next.createInstance;
