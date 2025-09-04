9cdaabfe8ad477f272cf5c464addf4497b2dbf4f
Andrew Jarrett <ahrjarrett@gmail.com>
2025-09-03 20:37:32 -0500

    chore: renames `keysFromSelector` to `keyFromSelector` so the name is less confusing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import i18next from './i18next.js';
 
-export { default as keysFromSelector } from './selector.js';
+export { default as keyFromSelector } from './selector.js';
 
 export default i18next;
 
